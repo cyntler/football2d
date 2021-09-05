7270355255ecb7e4a35d5e88148303b475c8d937
cyntler <damian@cyntler.com>
2021-09-05 17:46:47 +0200

    Add Babel and Webpack

---
+++ webpack.config.mjs
@@ -0,0 +1,27 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import HtmlPlugin from 'html-webpack-plugin';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: 'development',
+  entry: './src/preview/index.ts',
+  output: {
+    path: join(currentDir, 'preview'),
+    filename: 'bundle.[fullHash].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts'],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, use: ['babel-loader'] }],
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './src/preview/index.html',
+      inject: 'body',
+    }),
+  ],
+};
