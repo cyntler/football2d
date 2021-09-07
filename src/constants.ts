76f10f2ffc1ef5974c165e25acc0ae85c2ecd0a6
cyntler <damian@cyntler.com>
2021-09-07 20:18:20 +0200

    Add file with constants values

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const FIELD_WIDTH = 125;
+export const FIELD_HEIGHT = 85;
+export const FIELD_MARGIN = 50;
+
+export const FIELD_PRIMARY_COLOR = 'rgb(86, 140, 43)';
+export const FIELD_SECONDARY_COLOR = 'rgb(71, 117, 34)';
+export const FIELD_ACCENT_COLOR = 'rgb(255, 255, 255)';
